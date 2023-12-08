4e0046c688661d7e776be79951a55ffab9a81143
Swagam Dasgupta <swagam.dasgupta@gmail.com>
2023-12-08 15:35:18 +0530

    random arcs

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,26 +1,74 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
+// const degToRad = (deg) => {
+//   return deg*(Math.PI/180);
+// };
+
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min;
+// }
+
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-
-    const x = width*0.5;
-    const y = width*0.5;
-    const w = width*0.3;
-    const h = width*0.3;
-
-    context.translate(x, y);
-    context.rotate(0.3);
-
     context.fillStyle = 'black';
-    context.beginPath();
-    context.rect(-w*0.5, -h*0.5, w, h);
-    context.fill();
+
+    const num = 15;
+
+    const cx = width*0.5;
+    const cy = width*0.5;
+    const w = width*0.01;
+    const h = width*0.1;
+    const r = width*0.35;
+    let x, y;
+
+
+    for (let i=0; i<num; i++){
+
+      const slice = math.degToRad(360/num);
+      const angle = slice*i;
+
+      x = cx + r * Math.sin(angle);
+      // console.log(x);
+      y = cy + r * Math.cos(angle);
+      // console.log(y);
+
+      // context.beginPath();
+      // context.fillRect(x, y, w, h);
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+
+      // context.scale(randomRange(1, 3),1);
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
+
+      context.beginPath();
+      context.rect(-w*0.5, random.range(0, -h*0.5), w, h);
+      context.fill();
+      context.restore();
+
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5, 20);
+
+      context.beginPath();
+      context.arc(0, 0, r * random.range(0.7, 1.3), slice * random.range(1, -6), slice * random.range(1, 7));
+      context.stroke();
+      context.restore();
+
+
+    }
   };
 };
 
